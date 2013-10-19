c4fcb7656936afa1fa58e3ccfbed6a664fb72f75
Olexiy Zamkoviy <olexiy.z@gmail.com>
2013-10-19 18:43:19 +0300

    Initial commit, importing example applications from Weblocks

---
+++ simple-blog/conf/stores.lisp
@@ -0,0 +1,9 @@
+
+(in-package :simple-blog)
+
+;;; Multiple stores may be defined. The last defined store will be the
+;;; default.
+(defstore *blog-store* :prevalence
+  (merge-pathnames (make-pathname :directory '(:relative "data"))
+                   (asdf-system-directory :simple-blog)))
+
